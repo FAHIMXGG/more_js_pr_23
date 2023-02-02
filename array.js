bb784222d68767e2a6ce9873c5cf00cefd712ded
FAHIMXGG <ahasanulhaquefahim1@gmail.com>
2023-02-02 23:03:25 +0600

    1st

---
+++ array.js
@@ -0,0 +1,9 @@
+const country = 'Bangladesh';
+const age = 52;
+const isIndependent = true;
+const student = {id: 121, class: 11, name: 'Agun'};
+const friends = [13, 14, 11, 17, 21, 16, 15, 20];
+//concat
+const newFriendsAge = [12, 13, 11, 13]
+const allFriends = newFriendsAge.concat(friends);
+console. log(allFriends)
