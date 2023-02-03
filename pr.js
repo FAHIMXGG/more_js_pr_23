9f233f0fe353c800c36b69abed698d194e7e1a33
FAHIMXGG <ahasanulhaquefahim1@gmail.com>
2023-02-03 21:46:41 +0600

    finished

---
+++ pr.js
@@ -0,0 +1,21 @@
+// nayoks=["Bangla Bhai", "English Bhai","Korean Bhai ","Turkish Bhai "]
+// for (const nayok of nayoks) {
+//     console.log(nayok);
+//   }
+// console.log(nayoks);
+// let sum=0; 
+// for( let i = 0; i<=3;i++){ 
+// sum = sum + i; 
+// }
+// console.log(sum);
+// const myString = 'aytyt';
+// myString[2]='s'
+// console.log(myString.length);
+
+var array = ['a', 'b', 'c'];
+
+var replacement = 'x';
+
+array[array.length - 1] = replacement;
+
+console.log(array);
